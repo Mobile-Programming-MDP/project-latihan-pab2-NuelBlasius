a748b473c76a4175e98a1a85ff5984aede2710c3
NuelBlasius <161033492+NuelBlasius@users.noreply.github.com>
2025-03-12 20:22:24 +0700

    Update detail_screen.dart

--- pilem2/lib/screens/detail_screen.dart
+++ pilem2/lib/screens/detail_screen.dart
@@ -5,7 +5,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class DetailScreen extends StatefulWidget {
   final Movie movie;
-
   const DetailScreen({super.key, required this.movie});
 
   @override
@@ -30,93 +29,85 @@ class _DetailScreenState extends State<DetailScreen> {
 
   Future<void> _toggleFavorite() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _isFavorite = !_isFavorite;
-    });
+    setState(() => _isFavorite = !_isFavorite);
+
+    List<String> favoriteMovieIds = prefs.getStringList('favoriteMovies') ?? [];
     if (_isFavorite) {
-      final String movieJson = jsonEncode(widget.movie.toJson());
-      prefs.setString('movie_${widget.movie.id}', movieJson);
-      List<String> favoriteMovieIds = prefs.getStringList('favoriteMovies') ?? [];
+      prefs.setString('movie_${widget.movie.id}', jsonEncode(widget.movie.toJson()));
       favoriteMovieIds.add(widget.movie.id.toString());
-      prefs.setStringList('favoriteMovies', favoriteMovieIds);
     } else {
       prefs.remove('movie_${widget.movie.id}');
-      List<String> favoriteMovieIds = prefs.getStringList('favoriteMovies') ?? [];
       favoriteMovieIds.remove(widget.movie.id.toString());
-      prefs.setStringList('favoriteMovies', favoriteMovieIds);
     }
+    prefs.setStringList('favoriteMovies', favoriteMovieIds);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.movie.title),
-      ),
+      appBar: AppBar(title: Text(widget.movie.title)),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Stack(
-                children: [
-                  Image.network(
-                    'https://image.tmdb.org/t/p/w500${widget.movie.backdropPath}',
-                    height: 300,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                  Positioned(
-                    bottom: 8,
-                    right: 8,
-                    child: IconButton(
-                      icon: Icon(
-                        _isFavorite ? Icons.favorite : Icons.favorite_border,
-                        color: Colors.red,
-                      ),
-                      onPressed: _toggleFavorite,
-                    ),
-                  ),
-                ],
-              ),
+              _buildMovieImage(),
               const SizedBox(height: 20),
-              const Text(
-                'Overview:',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
+              _buildSectionTitle('Overview:'),
               const SizedBox(height: 10),
               Text(widget.movie.overview),
               const SizedBox(height: 20),
-              Row(
-                children: [
-                  const Icon(Icons.calendar_month, color: Colors.blue),
-                  const SizedBox(width: 10),
-                  const Text(
-                    'Release Date:',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(width: 10),
-                  Text(widget.movie.releaseDate),
-                ],
-              ),
+              _buildMovieDetail(Icons.calendar_month, 'Release Date:', widget.movie.releaseDate),
               const SizedBox(height: 20),
-              Row(
-                children: [
-                  const Icon(Icons.star, color: Colors.amber),
-                  const SizedBox(width: 10),
-                  const Text(
-                    'Rating:',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(width: 10),
-                  Text(widget.movie.voteAverage.toString()),
-                ],
-              ),
+              _buildMovieDetail(Icons.star, 'Rating:', widget.movie.voteAverage.toString()),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildMovieImage() {
+    return Stack(
+      children: [
+        Image.network(
+          'https://image.tmdb.org/t/p/w500${widget.movie.backdropPath}',
+          height: 300,
+          width: double.infinity,
+          fit: BoxFit.cover,
+        ),
+        Positioned(
+          bottom: 8,
+          right: 8,
+          child: IconButton(
+            icon: Icon(
+              _isFavorite ? Icons.favorite : Icons.favorite_border,
+              color: Colors.red,
+            ),
+            onPressed: _toggleFavorite,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSectionTitle(String title) {
+    return Text(
+      title,
+      style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget _buildMovieDetail(IconData icon, String label, String value) {
+    return Row(
+      children: [
+        Icon(icon, color: Colors.blue),
+        const SizedBox(width: 10),
+        Text(label, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        const SizedBox(width: 10),
+        Text(value),
+      ],
     );
   }
 }
